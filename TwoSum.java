94004d471d0a3b4916552841d061d0eb152fb267
Qi Yang <96786782+qyscode@users.noreply.github.com>
2026-02-07 17:03:59 +0800

    Annotate TwoSum.Java
    Explain algorithm

--- TwoSum.java
+++ TwoSum.java
@@ -12,18 +12,22 @@ public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> exploredMap = new HashMap<>();
 
+        // Add all numbers into memory
         for (int i = 0; i < nums.length; i++) {
             exploredMap.put(nums[i], i);
         }
 
+        // Iterate array again, check if complement is in memory
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (exploredMap.containsKey(complement)) {
-                if (exploredMap.get(complement) != i) {
+                if (exploredMap.get(complement) != i) { // Removes edge case of returning same index twice
                     return new int[] {i, exploredMap.get(complement)};
                 }
             }
         }
         throw new IllegalArgumentException("No two sum solution");
+
+        // This method works for duplicates in different indices
     }
 }
